c1e9eeea52bcde4b32837def7d98a0a251240e8b
Nagendra, Kruthika (kn454g) <kn454g@att.com>
2019-09-24 16:30:07 +0530

    Added Second Keyword

--- Sample1.java
+++ Sample1.java
@@ -4,6 +4,7 @@ public class Sample1 {
 	static String s = "         this is reply";
 	String new = "Added Value";
 	String first = "Added first";
+	String second = "Added second";
 	
 	
 	public static void main (String args[]) {
