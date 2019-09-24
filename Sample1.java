12aaaee4d4f977a0b661db5cc9b22e4049e870ae
Nagendra, Kruthika (kn454g) <kn454g@att.com>
2019-09-24 16:30:56 +0530

    Revert "Added Second Keyword"
    This reverts commit c1e9eeea52bcde4b32837def7d98a0a251240e8b.

--- Sample1.java
+++ Sample1.java
@@ -4,7 +4,6 @@ public class Sample1 {
 	static String s = "         this is reply";
 	String new = "Added Value";
 	String first = "Added first";
-	String second = "Added second";
 	
 	
 	public static void main (String args[]) {
