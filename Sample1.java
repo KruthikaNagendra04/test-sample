76cf9f51265fff247bbc51c84e4b65632c18a6a4
Nagendra, Kruthika (kn454g) <kn454g@att.com>
2019-09-24 17:11:54 +0530

    Added New sysout in new branch

--- Sample1.java
+++ Sample1.java
@@ -3,13 +3,13 @@ public class Sample1 {
 	
 	static String s = "         this is reply";
 	String new = "Added Value";
-	String first = "Added first";
-	String second = "Added second";
 	
 	
 	public static void main (String args[]) {
 		
 		System.out.println("s:"+s.trim());
+		System.out.println("new");
+		
 		
 	}
 	
